86537a349089cc19196b27eadb1c3e798878b289
Emek Sancar <emeksancar@gmail.com>
2018-05-19 15:46:27 +0300

    cart

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Models\Product;
-use App\Models\ProductDetail;
 use Illuminate\Database\Seeder;
+
 
 class ProductTableSeeder extends Seeder
 {
@@ -13,24 +13,23 @@ class ProductTableSeeder extends Seeder
      */
     public function run(Faker\Generator $faker)
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('products')->delete();
         DB::table('product_details')->delete();
 
         for ($i=0; $i<30; $i++) {
             $product_name = $faker->sentence(2);
-            $product = Product::create([
+             $product = Product::create([
                 'product_name' => $product_name,
                 'slug' => str_slug($product_name),
                 'description' => $faker->sentence(20),
-                'product_price' => $faker->randomFloat(3, 1, 20)
+                'price' => $faker->randomFloat(3, 1, 20)
             ]);
-            $detail = $product->detail()->create([
-                'show_slider' => rand(0,1),
-                'show_deal_of_the_day' => rand(0,1),
-                'show_on_sale' => rand(0,1)
-            ]);
+             $detail = $product->detail()->create([
+                 'show_slider' => rand(0,1),
+                 'show_on_sale' => rand(0,1),
+                 'show_deal_of_the_day' => rand(0,1),
+             ]);
         }
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
     }
 }
